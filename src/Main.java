52365397f45e4a18081b53d3fe663de038a5d28d
muchrizfanani <muchrizfanani@gmail.com>
2025-10-24 16:27:52 +0700

    menyimpan

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // 1. Membuat objek Scanner untuk membaca input
+
         Scanner scanner = new Scanner(System.in);
 
-        // 2. Meminta pengguna memasukkan bilangan
+
         System.out.print("Masukkan sebuah bilangan bulat: ");
 
-        // 3. Membaca bilangan yang dimasukkan
         int bilangan = scanner.nextInt();
 
-        // 4. Logika penentuan Genap/Ganjil
-        //    Bilangan genap adalah bilangan yang habis dibagi 2 (sisa baginya 0)
-        //    Operator '%' adalah operator modulus (menghitung sisa bagi)
         if (bilangan % 2 == 0) {
-            // Jika sisa baginya 0, maka genap
+
             System.out.println("Bilangan " + bilangan + " adalah GENAP.");
         } else {
-            // Jika sisa baginya bukan 0 (pasti 1), maka ganjil
+
             System.out.println("Bilangan " + bilangan + " adalah GANJIL.");
         }
 
-        // 5. Menutup objek Scanner setelah selesai
+
         scanner.close();
     }
 }
